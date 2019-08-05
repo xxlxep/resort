27b885755b4a852f5581e8e878ef1e290d6e597d
xxlxep <xxlxep@gmail.com>
2019-08-05 22:28:02 +0300

    Router-Dom added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import SingleRoom from './pages/SingleRoom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms/" component={Rooms} />
+        <Route path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
