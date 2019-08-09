e0fba88f352cb3f404685ee0f48989b72e6f66fa
xxlxep <xxlxep@gmail.com>
2019-08-09 15:52:34 +0300

    Slug passig to singleRoom.js through context

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,48 @@
 import React, { Component } from 'react';
-
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
 export default class Hello extends Component {
+  constructor(props) {
+    super(props);
+    //console.log(this.props);
+    this.state = { slug: this.props.match.params.slug, defaultBcg };
+  }
+  static contextType = RoomContext;
   render() {
-    return <div>Hello from singleroom page</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such room could be found</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capaciti,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
